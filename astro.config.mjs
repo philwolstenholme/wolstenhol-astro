630820055df1e36acc052fa6d900982e14d8c9df
Phil Wolstenholme <philip.wolstenholme@coop.co.uk>
2023-12-26 11:17:15 +0000

    further setup, format files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import alpinejs from "@astrojs/alpinejs";
 
@@ -8,5 +8,5 @@ import netlify from "@astrojs/netlify";
 export default defineConfig({
   integrations: [tailwind(), alpinejs()],
   output: "server",
-  adapter: netlify()
+  adapter: netlify(),
 });
