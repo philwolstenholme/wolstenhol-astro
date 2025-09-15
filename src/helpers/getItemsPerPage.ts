c37d56464526a5aa3770c220a7f07567406200e8
Phil Wolstenholme <phil@wolstenhol.me>
2025-09-15 23:25:40 +0100

    WIP

--- src/helpers/getItemsPerPage.ts
+++ src/helpers/getItemsPerPage.ts
@@ -3,7 +3,10 @@ const desktopItemsPerPage = 12;
 
 /** Uses the `Sec-CH-UA-Mobile` header if it's available to work out how many items to show per page */
 export const getItemsPerPage = (astroRequest?: Request) => {
-  if (!astroRequest) return desktopItemsPerPage;
+  if (!astroRequest) {
+    return desktopItemsPerPage;
+  }
+
   const header = astroRequest.headers.get("Sec-CH-UA-Mobile");
   return header === "?1" ? mobileItemsPerPage : desktopItemsPerPage;
 };
