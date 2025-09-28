4737e0b7dacc95b93b409794e722921633ab3de1
Phil Wolstenholme <phil@wolstenhol.me>
2025-09-28 15:26:28 +0100

    WIP

--- src/helpers/getItemsPerPage.ts
+++ src/helpers/getItemsPerPage.ts
@@ -1,4 +1,9 @@
-/** Uses the `Sec-CH-UA-Mobile` header if it's available to work out how many items to show per page. */
+import { UAParser } from "ua-parser-js";
+
+/**
+ * Uses the `Sec-CH-UA-Mobile` header if it's available to work out how many
+ * items to show per page.
+ */
 export const getItemsPerPage = ({
   astroRequest,
   mobileItemsPerPage = 5,
@@ -12,6 +17,25 @@ export const getItemsPerPage = ({
     return desktopItemsPerPage;
   }
 
-  const header = astroRequest.headers.get("Sec-CH-UA-Mobile");
-  return header === "?1" ? mobileItemsPerPage : desktopItemsPerPage;
+  // Prefer client hints if available.
+  const clientHintMobile = astroRequest.headers.get("Sec-CH-UA-Mobile");
+  if (clientHintMobile) {
+    return clientHintMobile === "?1" ? mobileItemsPerPage : desktopItemsPerPage;
+  }
+
+  // User agent sniff if we must.
+  const userAgent = astroRequest.headers.get("User-Agent");
+  if (userAgent) {
+    const { device } = UAParser(userAgent);
+    const smallScreenDeviceTypes: (typeof device.type)[] = [
+      "mobile",
+      "tablet",
+      "wearable",
+    ];
+    return smallScreenDeviceTypes.includes(device.type)
+      ? mobileItemsPerPage
+      : desktopItemsPerPage;
+  }
+
+  return desktopItemsPerPage;
 };
