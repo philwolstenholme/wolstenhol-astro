83aa3d51249bdbb1e15aa260735d7c889a4ab2de
Phil Wolstenholme <phil@wolstenhol.me>
2026-07-06 20:14:57 +0100

    Add debugging for index-mobile issue

--- src/helpers/detectMobile.ts
+++ src/helpers/detectMobile.ts
@@ -2,6 +2,7 @@ import { UAParser } from "ua-parser-js";
 
 export function detectMobile(request: Request): boolean {
   const clientHint = request.headers.get("Sec-CH-UA-Mobile");
+  console.log("Client Hint:", clientHint);
   if (clientHint !== null) {
     return clientHint === "?1";
   }
@@ -9,7 +10,9 @@ export function detectMobile(request: Request): boolean {
   const ua = request.headers.get("User-Agent");
   if (ua) {
     const { device } = UAParser(ua);
+    console.log("Device Type:", device.type);
     const mobileDeviceTypes: (typeof device.type)[] = ["mobile", "wearable"];
+    console.log("Is Mobile Device:", mobileDeviceTypes.includes(device.type));
     return mobileDeviceTypes.includes(device.type);
   }
 
